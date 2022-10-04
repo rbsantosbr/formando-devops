0cd65bbf1e6d7166b2dee3b4f8cce404fabad319
Roberto Santos <rbsantos85@gmail.com>
2022-10-03 22:54:23 -0300

    inicia qustão 3

---
+++ desafio-terraform/3/variable.tf
@@ -0,0 +1,13 @@
+variable "data" {
+  value = formatedate("DD-MM-YYYY",timestamp())
+}
+
+variable "div" {
+  type = number
+  default = 1
+}
+
+variable "nome" {
+  type = string
+  default = "Roberto"
+}
