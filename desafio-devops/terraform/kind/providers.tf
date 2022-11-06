1cadb7df7d337c8776787db7d4ab017c8946896f
Roberto Santos <rbsantos85@gmail.com>
2022-11-05 23:57:55 -0300

    atualiza estrutura

--- desafio-devops/terraform/kind/providers.tf
+++ desafio-devops/terraform/kind/providers.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    gitlab = {
-      source = "gitlabhq/gitlab"
-      version = "3.18.0"
-    }
     kind = {
       source = "kyma-incubator/kind"
       version = "0.0.11"
