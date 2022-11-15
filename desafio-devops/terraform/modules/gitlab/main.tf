21778e118021b47afed1c4eff7eea800157a0ee3
Roberto Santos <rbsantos85@gmail.com>
2022-11-15 18:48:50 -0300

    Adiciona resource gitlab_repository_file

--- desafio-devops/terraform/modules/gitlab/main.tf
+++ desafio-devops/terraform/modules/gitlab/main.tf
@@ -5,3 +5,14 @@ resource "gitlab_project" "desafio-devops-projects" {
   visibility_level = "public"
   default_branch = "main"
 }
+
+resource "gitlab_repository_file" "readme" {
+  project   = gitlab_project.desafio-devops-projects.id
+  file_path = "README.md"
+  branch    = "main"
+  // content will be auto base64 encoded
+  content        = "${var.project_name}"
+  author_email   = "rbsantos85@gmail.com"
+  author_name    = "Roberto Santos"
+  commit_message = "feature: add readme file"
+}
