f24f0ae5615c6fd38c40f2824d01f3ff7e6ef183
Roberto Santos <rbsantos85@gmail.com>
2022-11-15 18:54:15 -0300

    Remove resource gitlab_repository_file

--- desafio-devops/terraform/modules/gitlab/main.tf
+++ desafio-devops/terraform/modules/gitlab/main.tf
@@ -5,14 +5,3 @@ resource "gitlab_project" "desafio-devops-projects" {
   visibility_level = "public"
   default_branch = "main"
 }
-
-resource "gitlab_repository_file" "readme" {
-  project   = gitlab_project.desafio-devops-projects.id
-  file_path = "README.md"
-  branch    = "main"
-  // content will be auto base64 encoded
-  content        = "${var.project_name}"
-  author_email   = "rbsantos85@gmail.com"
-  author_name    = "Roberto Santos"
-  commit_message = "feature: add readme file"
-}
